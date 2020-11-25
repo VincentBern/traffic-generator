cdf91b87c33b329b0d326692408e5c6ef542a053
wnijmeijer <wnijmeijer@coveo.com>
2020-11-25 14:59:29 +0100

    Initial

---
+++ pages/Coveo.js
@@ -0,0 +1,447 @@
+let prevRND = 1;
+let currentPause = 1000;
+let currentFacet = "";
+
+var coveoCommands = {
+  //*******************************************************
+  //getRandomInt, for getting random results
+  //*******************************************************
+  getRandomInt: function (min, max) {
+    let number = Math.floor(Math.random() * Math.floor(10));
+    if (number < min) number = min;
+    if (number > max) number = max;
+    while (prevRND == number) number = this.getRandomInt(min, max);
+    return number;
+  },
+  getMaxResults: function (selector = "") {
+    selector += " .CoveoResult";
+    let max = 0;
+    this.elements("css selector", selector, (labels) => {
+      max = labels.length;
+    });
+    return max;
+  },
+  //*******************************************************
+  //c_setPause, to set timing between events
+  //*******************************************************
+  c_setPause: function (nr) {
+    currentPause = nr;
+  },
+  //*******************************************************
+  //c_makeVisible, make elements visible
+  //*******************************************************
+  c_makeVisible: function (selector) {
+    this.api.execute(
+      function (selector) {
+        var element = document.querySelectorAll(selector);
+        element.forEach((el) => {
+          el.setAttribute("style", "display:inherit");
+        });
+      },
+      [selector]
+    );
+  },
+  //*******************************************************
+  //c_search, search for text. Do not click on submit
+  //*******************************************************
+  c_search: function (text, searchinterface = "", specific = "") {
+    let selector = "";
+    if (searchinterface != "") {
+      searchinterface = searchinterface + " ";
+    }
+    selector += ".CoveoSearchbox .magic-box-input > input";
+    if (specific != "") {
+      selector = specific;
+    }
+    return this.waitForElementVisible(searchinterface + selector)
+      .clearValue(searchinterface + selector)
+      .setValue(searchinterface + selector, text)
+      .pause(currentPause);
+  },
+  //*******************************************************
+  //c_searchAndSubmit, search for text and click on submit
+  //*******************************************************
+  c_searchAndSubmit: function (text, searchinterface = "", specific = "") {
+    let selector = "";
+    if (searchinterface != "") {
+      searchinterface = searchinterface + " ";
+    }
+    selector += ".CoveoSearchbox .magic-box-input > input";
+    if (specific != "") {
+      selector = specific;
+    }
+    return this.waitForElementVisible(searchinterface + selector)
+      .clearValue(searchinterface + selector)
+      .setValue(searchinterface + selector, text)
+      .c_click(searchinterface + ".CoveoSearchButton")
+      .pause(currentPause);
+  },
+  //*******************************************************
+  //c_searchAndClickSuggestion, search for text and click on NR suggestion
+  //*******************************************************
+  c_searchAndClickSuggestion: function (
+    text,
+    searchinterface = "",
+    nr = 0,
+    specific = "",
+    specificSuggestion = ""
+  ) {
+    let selector = "";
+    if (searchinterface != "") {
+      searchinterface = searchinterface + " ";
+    }
+    selector += ".CoveoSearchbox .magic-box-input > input";
+    if (specific != "") {
+      selector = specific;
+    }
+    let selectorSuggestion = "#magic-box-suggestion-" + nr + "";
+    if (specificSuggestion != "") {
+      selectorSuggestion = specificSuggestion;
+    }
+    return this.waitForElementVisible(searchinterface + selector)
+      .clearValue(searchinterface + selector)
+
+      .setValue(searchinterface + selector, text)
+      .click(selectorSuggestion)
+      .pause(currentPause);
+  },
+  //*******************************************************
+  //c_click, click on an element
+  //*******************************************************
+  c_click: function (selector) {
+    return this.waitForElementVisible(
+      {
+        selector: selector,
+        abortOnFailure: false,
+        suppressNotFoundErrors: true,
+      },
+      5000
+    )
+      .moveToElement(selector, 10, 10)
+      .setValue(selector, "")
+      .pause(200)
+      .click({
+        selector: selector,
+        abortOnFailure: false,
+        suppressNotFoundErrors: true,
+      })
+      .pause(currentPause);
+  },
+  //*******************************************************
+  //c_click, click on an element
+  //*******************************************************
+  c_moveToElement: function (selector, x, y) {
+    return this.waitForElementVisible(
+      {
+        selector: selector,
+        abortOnFailure: false,
+        suppressNotFoundErrors: true,
+      },
+      5000
+    )
+      .moveToElement(selector, x, y)
+      .pause(currentPause);
+  },
+  //*******************************************************
+  //c_setValue, set value of an element
+  //*******************************************************
+  c_setValue: function (selector, val) {
+    return this.waitForElementVisible(selector, 5000)
+      .moveToElement(selector, 10, 10)
+      .setValue(selector, val);
+  },
+  //*******************************************************
+  //c_selectFacet, select random facet from field  or take the nr requested
+  //*******************************************************
+  c_selectFacet: function (field, nr = "RND", max = 5) {
+    if (nr == "RND") {
+      nr = this.getRandomInt(1, max);
+    }
+    let selector =
+      '.CoveoFacet[data-field="' + field + '"] > ul > li:nth-child(' + nr + ")";
+    currentFacet = selector;
+    return this.waitForElementVisible(selector, 5000)
+      .moveToElement(selector, 10, 10)
+      .pause(200)
+      .click({
+        selector: selector,
+        abortOnFailure: false,
+        suppressNotFoundErrors: true,
+      })
+      .pause(currentPause);
+  },
+  //*******************************************************
+  //c_selectDynamicFacet, select random facet from field or take the nr requested
+  //*******************************************************
+  c_selectDynamicFacet: function (field, nr = "RND", max = 5) {
+    if (nr == "RND") {
+      nr = this.getRandomInt(1, max);
+    }
+    let selector =
+      '.CoveoDynamicFacet[data-field="' +
+      field +
+      '"] > ul > li:nth-child(' +
+      nr +
+      ")";
+    currentFacet = selector;
+    return this.waitForElementVisible(selector, 5000)
+      .moveToElement(selector, 10, 10)
+      .pause(200)
+      .click({
+        selector: selector,
+        abortOnFailure: false,
+        suppressNotFoundErrors: true,
+      })
+      .pause(currentPause);
+  },
+  //*******************************************************
+  //c_deselectFacet, deselect the facet value, earlier selected with selectFacet/selectDynamicFacet
+  //*******************************************************
+  c_deselectFacet: function () {
+    //Deselects the previous (random facet selected)
+    let selector = currentFacet;
+    return this.waitForElementVisible(selector, 5000)
+      .moveToElement(selector, 10, 10)
+      .pause(200)
+      .click({
+        selector: selector,
+        abortOnFailure: false,
+        suppressNotFoundErrors: true,
+      })
+      .pause(currentPause);
+  },
+  //*******************************************************
+  //c_selectFacetValue, select facet with specific value
+  //*******************************************************
+  c_selectFacetValue: function (value) {
+    let selector = 'li[data-value="' + value + '"]';
+    return this.waitForElementVisible(selector, 5000)
+      .moveToElement(selector, 10, 10)
+      .pause(200)
+      .click({
+        selector: selector,
+        abortOnFailure: false,
+        suppressNotFoundErrors: true,
+      })
+      .pause(currentPause);
+  },
+  //*******************************************************
+  //c_deSelectFacetValue, deselect facet with specific value
+  //*******************************************************
+  c_deSelectFacetValue: function (value) {
+    let selector = 'li[data-value="' + value + '"]';
+    return this.waitForElementVisible(selector, 5000)
+      .moveToElement(selector, 10, 10)
+      .pause(200)
+      .click({
+        selector: selector,
+        abortOnFailure: false,
+        suppressNotFoundErrors: true,
+      })
+      .pause(currentPause);
+  },
+  //*******************************************************
+  //c_clickResult, click on result nr (RND or number) with resultlist id. Choose between 1 and max of occurences for RND
+  //*******************************************************
+  c_clickResult: function (
+    nr = "RND",
+    resultlist = "",
+    max = 5,
+    specific = ""
+  ) {
+    let selector = "";
+    if (resultlist != "") {
+      selector = resultlist + " ";
+    }
+    if (nr == "RND") {
+      max = this.getMaxResults(selector);
+      nr = this.getRandomInt(1, max);
+    }
+    selector +=
+      ".CoveoResultList > div > div:nth-child(" + nr + ") .CoveoResultLink";
+    if (specific != "") {
+      selector = specific;
+    }
+    return this.waitForElementVisible(selector, 5000)
+      .moveToElement(selector, 10, 10)
+      .pause(200)
+      .click({
+        selector: selector,
+        abortOnFailure: false,
+        suppressNotFoundErrors: true,
+      })
+      .pause(currentPause);
+  },
+  //*******************************************************
+  //c_clickRecommendation, click on recommendation nr (RND or number) with resultlist id. Choose between 1 and max of occurences for RND
+  //*******************************************************
+  c_clickRecommendation: function (
+    nr = "RND",
+    resultlist = "",
+    max = 3,
+    specific = ""
+  ) {
+    let selector = "";
+    if (resultlist != "") {
+      selector = resultlist + " ";
+    }
+    if (nr == "RND") {
+      max = this.getMaxResults(selector);
+      nr = this.getRandomInt(1, max);
+    }
+    selector +=
+      ".CoveoResultList > div > div:nth-child(" + nr + ")  .CoveoResultLink";
+    if (specific != "") {
+      selector = specific;
+    }
+    return this.waitForElementVisible({
+      selector: selector,
+      abortOnFailure: false,
+      timeout: 5000,
+    })
+      .moveToElement(selector, 10, 10)
+      .pause(200)
+      .click({
+        selector: selector,
+        abortOnFailure: false,
+        suppressNotFoundErrors: true,
+      })
+      .pause(currentPause);
+  },
+  //*******************************************************
+  //c_clickQuickview, click on quickview nr (RND or number) with resultlist id. Choose between 1 and max of occurences for RND
+  // if autoclose is set, quickview will close automatically
+  //*******************************************************
+  c_clickQuickview: function (
+    nr = "RND",
+    resultlist = "",
+    max = 5,
+    autoclose = true
+  ) {
+    let selector = "";
+    if (resultlist != "") {
+      selector = resultlist + " ";
+    }
+    if (nr == "RND") {
+      max = this.getMaxResults(selector);
+      nr = this.getRandomInt(1, max);
+    }
+    selector +=
+      ".CoveoResultList > div > div:nth-child(" + nr + ")  .CoveoQuickview";
+    this.waitForElementVisible(selector, 5000)
+      .moveToElement(selector, 10, 10)
+      .pause(200)
+      .click({
+        selector: selector,
+        abortOnFailure: false,
+        suppressNotFoundErrors: true,
+      })
+      .pause(currentPause);
+    if (autoclose) this.c_closeQuickview();
+  },
+  //*******************************************************
+  //c_closeQuickview, close the quickview window
+  //*******************************************************
+  c_closeQuickview: function () {
+    let selector =
+      "div.coveo-modal-container.coveo-opened.coveo-mod-big.coveo-quick-view.coveo-mod-fade-in-scale > div > header > span";
+    return this.waitForElementVisible(selector, 5000)
+      .moveToElement(selector, 10, 10)
+      .pause(200)
+      .click({
+        selector: selector,
+        abortOnFailure: false,
+        suppressNotFoundErrors: true,
+      })
+
+      .pause(currentPause);
+  },
+  //*******************************************************
+  //c_selectTab, tab to select (using the caption)
+  //*******************************************************
+  c_selectTab: function (caption) {
+    let selector = '.CoveoTab[data-caption="' + caption + '"]';
+    return this.waitForElementVisible(selector, 5000)
+      .moveToElement(selector, 10, 10)
+      .pause(200)
+      .click({
+        selector: selector,
+        abortOnFailure: false,
+        suppressNotFoundErrors: true,
+      })
+
+      .pause(currentPause);
+  },
+  //*******************************************************
+  //c_loginOffice
+  //*******************************************************
+  c_loginOffice: function (user, pass) {
+    this.c_click("#loginWithOffice365")
+      .c_setValue("#i0116", user)
+      .c_click("#idSIButton9")
+      .c_setValue("#i0118", pass)
+      .c_click("#idSIButton9")
+      .c_click("#idBtn_Back");
+  },
+  c_waitForElement: function (selector, callback) {
+    let tries = 0;
+    console.log("CHECK FOR ELEMENT " + selector + " (" + tries + ")");
+    this.waitForElementVisible(selector, 4000, false, function (result) {
+      if (result.value && result.value == true) {
+        console.log("Element is there... " + selector);
+        callback(true);
+        return;
+      } else {
+        console.log("Element is NOT there... , retry " + selector);
+        this.pause(200);
+        this.moveToElement(selector, 10, 10);
+        tries += 1;
+        if (tries < 5) this.c_waitForElement(selector, callback);
+      }
+    });
+  },
+  c_clickv2: function (selector) {
+    let _this = this;
+    this.c_waitForElement(selector, function (result) {
+      if (result) {
+        console.log("Click on Element " + selector);
+        _this.click({
+          selector: selector,
+          abortOnFailure: false,
+          suppressNotFoundErrors: true,
+        });
+      }
+    });
+  },
+  c_setValuev2: function (selector, val) {
+    let _this = this;
+    this.c_waitForElement(selector, function (result) {
+      if (result) {
+        _this.setValue(selector, val);
+      }
+    });
+  },
+  //*******************************************************
+  //c_loginOfficev2
+  //*******************************************************
+  c_loginOfficev2: function (user, pass) {
+    this.c_clickv2("#loginWithOffice365");
+    this.c_setValuev2("#i0116", user);
+    this.c_clickv2("#idSIButton9");
+    this.c_setValuev2("#i0118", pass);
+    this.c_clickv2("#idSIButton9");
+    this.c_clickv2("#idBtn_Back");
+  },
+};
+
+module.exports = {
+  commands: [coveoCommands],
+  elements: {
+    searchbox: {
+      selector: "div.magic-box-input > input[type=text]",
+    },
+    submit: {
+      selector: ".CoveoSearchButton",
+    },
+  },
+};
---
+++ src/basic.js
@@ -0,0 +1,310 @@
+describe("WIMS", function () {
+  //Keep browser open when fails
+  this.endSessionOnFail = false;
+  this.abortOnElementLocateError = false;
+
+  test("Workplace2", function (browser) {
+    var coveo = browser.page.Coveo();
+    browser.url(
+      "https://search.cloud.coveo.com/pages/workplacedemoqjjnc2v7/full2"
+    );
+    coveo.c_loginOfficev2("adelev@M365x988456.onmicrosoft.com", "&Qwerty 123");
+    browser.pause(5000);
+  });
+
+  test("Philips", function (browser) {
+    var coveo = browser.page.Coveo();
+    browser.url(
+      "https://search-eu.cloud.coveo.com/pages/philips5pi3khvx/Demo#q=one%20blade&first=10&t=All&sort=relevancy"
+    );
+    browser.pause(5000);
+  }),
+    test("subzero", function (browser) {
+      var coveo = browser.page.Coveo();
+      //Set pause between events to 1 second
+      browser.url("https://www.subzero-wolf.com/");
+      coveo.c_click(".visitNASiteHandler");
+      //Open search box
+      coveo.c_click("#search-trigger");
+      coveo.c_search(
+        "grill",
+        "",
+        "#spotlightSearch .CoveoQuerybox .magic-box-input > input"
+      );
+      browser.keys(browser.Keys.ENTER);
+
+      browser.pause(3000);
+      coveo.c_moveToElement("article:nth-child(2)", 10, 10);
+      browser.pause(2000);
+      coveo.c_moveToElement("article:nth-child(3)", 10, 10);
+      browser.pause(2000);
+      coveo.c_moveToElement("article:nth-child(4)", 10, 10);
+      browser.pause(5000);
+
+      coveo.c_setPause(1000);
+      browser.url("https://www.enbridge.com/about-us");
+
+      coveo.c_search("gas", "", "#mainSearch");
+      browser.keys(browser.Keys.ENTER);
+      browser.pause(1000);
+    });
+
+  test("enbridge", function (browser) {
+    var coveo = browser.page.Coveo();
+    //Set pause between events to 1 second
+    browser.url(
+      "https://www.enbridge.com/about-us/natural-gas-transmission-and-midstream"
+    );
+    browser.pause(3000);
+    coveo.c_moveToElement("article:nth-child(2)", 10, 10);
+    browser.pause(2000);
+    coveo.c_moveToElement("article:nth-child(3)", 10, 10);
+    browser.pause(2000);
+    coveo.c_moveToElement("article:nth-child(4)", 10, 10);
+    browser.pause(5000);
+
+    coveo.c_setPause(1000);
+    browser.url("https://www.enbridge.com/about-us");
+
+    coveo.c_search("gas", "", "#mainSearch");
+    browser.keys(browser.Keys.ENTER);
+    browser.pause(1000);
+
+    coveo.c_moveToElement(
+      "#search > div:nth-child(3) > div > div > div.coveo-results-column.search-results > div.CoveoResultList > div.results-container.coveo-result-list-container > div:nth-child(1) > div.small-22.small-centered.medium-uncentered.medium-24.large-8.xlarge-12.medium-wide-only.xlarge-wide-up.columns > article",
+      20,
+      20
+    );
+    browser.pause(1000);
+    coveo.c_click(
+      "#search > div:nth-child(3) > div > div > div.coveo-results-column.search-results > div.CoveoResultList > div.results-container.coveo-result-list-container > div:nth-child(1) > div.small-22.small-centered.medium-uncentered.medium-24.large-8.medium-wide-only.xlarge-wide-up.xlarge-12.columns > article > div.tile__cta > a"
+    );
+    //************************************************************
+    //Could be that current window is switched
+    //************************************************************
+    let closeCurrentWindow = false;
+    browser.windowHandles(function (result) {
+      if (result.value.length == 2) {
+        handle = result.value[1];
+        browser.switchWindow(handle);
+        closeCurrentWindow = true;
+      }
+    });
+    //right column
+    browser.pause(2000);
+    //We first need to move to the element, then the result link will appear
+    coveo.c_moveToElement("article:nth-child(2)", 10, 10);
+    browser.pause(2000);
+    coveo.c_click("article:nth-child(2) > div.tile__cta > a");
+
+    browser.pause(2000);
+    //Close the new window
+    if (closeCurrentWindow) {
+      browser.closeWindow();
+      browser.windowHandles(function (result) {
+        handle = result.value[0];
+        browser.switchWindow(handle);
+      });
+    }
+
+    browser.url(
+      "https://www.enbridge.com/about-us/natural-gas-transmission-and-midstream"
+    );
+    browser.pause(2000);
+    coveo.c_moveToElement("article:nth-child(2)", 10, 10);
+    browser.pause(2000);
+    coveo.c_moveToElement("article:nth-child(3)", 10, 10);
+    browser.pause(2000);
+    coveo.c_moveToElement("article:nth-child(4)", 10, 10);
+    browser.pause(5000);
+
+    browser.end();
+  });
+
+  test("step b: the gym search", function (browser) {
+    var coveo = browser.page.Coveo();
+    //Set pause between events to 1 second
+    coveo.c_setPause(1000);
+    browser.url("https://thegym.coveodemo.com/");
+
+    coveo.c_searchAndClickSuggestion(
+      "pan",
+      "",
+      2,
+      "#search-input",
+      "#form-search > div.search-field-wrap > div > div:nth-child(2)"
+    );
+    coveo.c_selectFacetValue("M");
+    coveo.c_clickResult(
+      "2",
+      "",
+      5,
+      "#coveo-result-list4 > div > div:nth-child(2) .card__item-price"
+    );
+    browser.pause(1000);
+    //Add to cart
+    coveo.c_click("form > div > button");
+    browser.pause(2000);
+    //Click on recommendation
+    coveo.c_clickRecommendation(
+      "",
+      "",
+      3,
+      "#homeProductsPopular > div.CoveoResultList > div > div > div > div:nth-child(3) > div > div > div > a > div.card__item-price"
+    );
+    //New search
+    coveo.c_search("gloves", "", "#search-input");
+    //Hit enter
+    browser.keys(browser.Keys.ENTER);
+    browser.pause(3000);
+
+    browser.end();
+  });
+
+  test("Workplace", function (browser) {
+    var coveo = browser.page.Coveo();
+    browser.url(
+      "https://search.cloud.coveo.com/pages/workplacedemoqjjnc2v7/full2"
+    );
+    coveo.c_loginOffice("adelev@M365x988456.onmicrosoft.com", "&Qwerty 123");
+    browser.pause(2000);
+    coveo.c_setValue("#ProfilesDropdown", "Mark 8");
+    browser.pause(2000);
+    coveo.c_makeVisible(".CoveoQuickview");
+    coveo.c_makeVisible(".CoveoEditWidget");
+    //click on quickview widgets
+    coveo.c_clickQuickview("1", "#Rec2");
+    coveo.c_clickQuickview("2", "#Rec1");
+    coveo.c_click("#teamwidget > div > div.coveo-facet-settings-more");
+    coveo.c_click("body");
+    coveo.c_click("#selectedwidget > div > div.coveo-facet-settings-more");
+    coveo.c_click("body");
+    coveo.c_click("#mySidePeople");
+    coveo.c_click("#myDashboard");
+    coveo.c_searchAndSubmit("mark 8");
+    browser.pause(5000);
+    //coveo.waitForElementVisible("@searchbox");
+    //coveo.search("exchange");
+    browser.end();
+  });
+
+  test("step b: salesforce search", function (browser) {
+    var coveo = browser.page.Coveo();
+    //Set pause between events to 1 second
+    coveo.c_setPause(1000);
+    browser.url("https://help.salesforce.com/home");
+    //Get rid of pop up consent
+    browser.pause(1000);
+    coveo.c_click("#onetrust-accept-btn-handler");
+    browser.pause(1000);
+
+    coveo.c_searchAndClickSuggestion("exch", "#tds-header-search", 2);
+    coveo.c_search("exchange", "#tds-header-search");
+    coveo.c_click("#tds-header-search > div > span");
+    //Random facets
+    coveo.c_selectFacet("@objecttype");
+    coveo.c_deselectFacet();
+    coveo.c_selectTab("Answers");
+    coveo.c_selectTab("Articles");
+    coveo.c_click(
+      "#TopicFacet > ul > li:nth-child(1) > div.coveo-has-childs-toggle > span.coveo-hierarchical-facet-expand"
+    );
+    coveo.c_selectFacetValue(
+      "All|General Salesforce Functionality|Desktop Add-Ons"
+    );
+    coveo.c_deSelectFacetValue(
+      "All|General Salesforce Functionality|Desktop Add-Ons"
+    );
+
+    coveo.c_clickResult("1");
+    coveo.c_clickRecommendation("2", "#recommendedtraining_articleview_docs");
+    browser.end();
+  });
+
+  test("step b: bmc search", function (browser) {
+    var coveo = browser.page.Coveo();
+    //Set pause between events to 1 second
+    coveo.c_setPause(1000);
+    browser.url("https://www.bmc.com/support/resources/support-search.html");
+    //Get rid of pop up consent
+    browser.pause(1000);
+    browser.getAttribute(".truste_box_overlay iframe", "id", (result) => {
+      browser.pause(1000);
+      browser.frame(result.value).click(".pdynamicbutton > a");
+      browser.frame(result.value).click("#gwt-debug-close_id");
+    });
+
+    browser.click("body");
+    browser.pause(1000);
+
+    coveo.c_searchAndClickSuggestion("exch", "#supprt-search-input", 2);
+    coveo.c_searchAndSubmit("exchange", "#supprt-search-input");
+    //Random facets
+    coveo.c_selectFacet("@bmcproductname");
+    coveo.c_deselectFacet();
+    coveo.c_selectFacet("@bmcproductname");
+    coveo.c_deselectFacet();
+    coveo.c_clickQuickview("1");
+    coveo.c_clickQuickview("2");
+    coveo.c_clickResult("1");
+    coveo.c_clickRecommendation("RND");
+    browser.end();
+  });
+
+  test("step b: coveo search", function (browser) {
+    var coveo = browser.page.Coveo();
+    //Set pause between events to 1 second
+    coveo.c_setPause(1000);
+    browser.url(
+      "https://connect.coveo.com/s/global-search/%40uri#t=All&sort=relevancy"
+    );
+
+    coveo.c_searchAndClickSuggestion("exch", "#unifiedSearchPage", 2);
+    coveo.c_searchAndSubmit("exchange", "#unifiedSearchPage");
+    coveo.c_makeVisible(".CoveoQuickview");
+    coveo.c_selectFacetValue("Newsroom");
+    browser.pause(1000);
+    coveo.c_deSelectFacetValue("Newsroom");
+    browser.pause(5000);
+    //Random facets
+    coveo.c_selectFacet("@commonresourcetype");
+    coveo.c_deselectFacet();
+    coveo.c_selectFacet("@commonplatformcomponent");
+    coveo.c_deselectFacet();
+    coveo.c_clickQuickview("1");
+    coveo.c_clickQuickview("2");
+    coveo.c_clickResult("1");
+    coveo.c_clickRecommendation("RND");
+    browser.end();
+  });
+  test("step b: motorola search", function (browser) {
+    var coveo = browser.page.Coveo();
+    //Set pause between events to 1 second
+    coveo.c_setPause(1000);
+    browser.url(
+      "https://www.motorolasolutions.com/en_us/search.html#t=Tab_All"
+    );
+    browser.pause(1000);
+    //Get rid of pop up consent
+    browser.getAttribute(".truste_box_overlay iframe", "id", (result) => {
+      browser.pause(1000);
+      browser.frame(result.value).click(".pdynamicbutton > a");
+    });
+    browser.pause(1000);
+    //Continue with search
+    coveo.c_searchAndClickSuggestion("ret", "#coveoSearchInterface_main", 2);
+    browser.pause(5000);
+    coveo.c_searchAndSubmit("retail", "#coveoSearchInterface_main");
+    browser.pause(5000);
+    coveo.c_selectFacetValue("MSI.com DAM");
+    browser.pause(1000);
+    coveo.c_deSelectFacetValue("MSI.com DAM");
+    browser.pause(5000);
+    //Random facets
+    coveo.c_selectFacet("@source");
+    coveo.c_deselectFacet();
+    coveo.c_selectTab("Tab_PagesDocs__caption");
+    coveo.c_clickResult("1");
+    browser.end();
+  });
+});
