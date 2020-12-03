e0e01f8d28a89ca3e88d9175bbdeab9e25791825
JDevost <jdevost@coveo.com>
2020-12-03 11:03:55 -0500

    formatting

--- commands/setCoveoSpy.js
+++ commands/setCoveoSpy.js
@@ -3,13 +3,9 @@ module.exports = class CustomCommand {
     return new Promise(resolve => {
       this.api.executeAsync(function (selector, done) {
           Coveo.$(document.querySelector(selector, Coveo.SearchInterface)).on('querySuccess', (e, args) => {
-            const {
-              query
-            } = args;
-            const {
-              facets,
-              groupByResults
-            } = args.results;
+            const { query } = args;
+            const { facets, groupByResults } = args.results;
+
             window._LAST_COVEO_RESPONSE = JSON.stringify({
               query,
               facets,
