43a11fe65af9ce2b62050000e7fe6be1ba767421
JDevost <jdevost@coveo.com>
2021-02-12 10:46:12 -0500

    add scenarios for Dryers

--- src/storefront_bestbuy.js
+++ src/storefront_bestbuy.js
@@ -8,17 +8,38 @@ describe("BestBuy (Storefront headless)", function () {
   beforeEach(async function (browser) {
     await browser.resizeWindow(1565, 1237);
     // await browser.url("https://genericstore.coveodemo.com/");
-    await browser.url("http://localhost:3000/searchPage");
+    await browser.url("http://localhost:3000");
   });
 
-  test("washer DNE", async function (browser) {
+  test("DNE - Washer", async function (browser) {
     await browser.CoveoSearch("Washer");
 
     await browser.CoveoSelectFacetValue("Appliances");
     await browser.CoveoSelectFacetValue("Washers & Dryers");
     await browser.CoveoSelectFacetValue("Washing Machines");
 
+    await browser.CoveoOpenResult("RND");
+  });
+
+  test("DNE - Dryer", async function (browser) {
     await browser.CoveoSearch("Dryer");
+
+    await browser.CoveoSelectFacetValue("Appliances");
+    await browser.CoveoSelectFacetValue("Washers & Dryers");
+    await browser.CoveoSelectFacetValue("Dryers");
+
+    await browser.CoveoOpenResult("RND");
+  });
+
+  test("DNE - Hand Dryer", async function (browser) {
+    await browser.CoveoSearch("Hand Dryer");
+
+    await browser.CoveoSelectFacetValue("Appliances");
+    await browser.CoveoSelectFacetValue("Small Kitchen Appliances");
+    await browser.CoveoSelectFacetValue("Kitchen Gadgets");
+    await browser.CoveoSelectFacetValue("Soap Dispensers & Hand Dryers");
+
+    await browser.CoveoOpenResult("RND");
   });
 
   afterEach(browser => browser.end());
