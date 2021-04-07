288ce1ab1dafda18c053b16e47d646b9adf5ec2e
Sebastian Alvarez salvarez@coveo.com <salvarez@coveo.com>
2021-04-07 17:00:33 -0400

    remove commented lines

--- src/Examples/_genericStore.js
+++ src/Examples/_genericStore.js
@@ -10,25 +10,25 @@ describe("Running Search Journeys", function () {
     await browser.url("https://genericstore.coveodemo.com/");
   });
 
-  // test("Search", async function (browser) {
-  //   await browser.CoveoSearch("laptop");
-  // });
+  test("Search", async function (browser) {
+    await browser.CoveoSearch("laptop");
+  });
 
-  // test("Search and click random result", async function (browser) {
-  //   await browser.CoveoSearch("laptop");
-  //   await browser.CoveoClickResultByNumber();
-  // });
+  test("Search and click random result", async function (browser) {
+    await browser.CoveoSearch("laptop");
+    await browser.CoveoClickResultByNumber();
+  });
 
-  // test("Search and click (using result title)", async function (browser) {
-  //   await browser.CoveoSearch("laptop");
-  //   await browser.CoveoClickResultByText("14.0\" Laptop - Intel Celeron N4020 - 4GB Memory - 64GB eMMC - Star Black");
-  // });
+  test("Search and click (using result title)", async function (browser) {
+    await browser.CoveoSearch("laptop");
+    await browser.CoveoClickResultByText("14.0\" Laptop - Intel Celeron N4020 - 4GB Memory - 64GB eMMC - Star Black");
+  });
 
-  // test("Search, filter by category facet and click", async function (browser) {
-  //   await browser.CoveoSearch("laptop");
-  //   await browser.CoveoSelectFacetValueByText(["computers & tablets", "laptops", "All Laptops", "MacBooks"]);
-  //   await browser.CoveoClickResultByNumber();
-  // });
+  test("Search, filter by category facet and click", async function (browser) {
+    await browser.CoveoSearch("laptop");
+    await browser.CoveoSelectFacetValueByText(["computers & tablets", "laptops", "All Laptops", "MacBooks"]);
+    await browser.CoveoClickResultByNumber();
+  });
 
   test("Search and click (using result title and set pagination depth)", async function (browser) {
     // Go through 20 pages (fetch more results) to find product. Default 5
@@ -46,13 +46,13 @@ describe("Running Search Journeys", function () {
     );
   });
 
-  // test("Search, click and Add to Cart", async function (browser) {
-  //   await browser.CoveoSearch("laptop");
-  //   await browser.CoveoClickResultByText("Neo 15.6\" Laptop - Intel Core i5 - 8GB Memory - 1TB HDD");
-  //   await browser.waitForElementVisible(GenericStoreSelectors.productDetailPage.container);
-  //   await browser.click(GenericStoreSelectors.productDetailPage.addToCart);
-  //   await browser.pause(1000);
-  // });
+  test("Search, click and Add to Cart", async function (browser) {
+    await browser.CoveoSearch("laptop");
+    await browser.CoveoClickResultByText("Neo 15.6\" Laptop - Intel Core i5 - 8GB Memory - 1TB HDD");
+    await browser.waitForElementVisible(GenericStoreSelectors.productDetailPage.container);
+    await browser.click(GenericStoreSelectors.productDetailPage.addToCart);
+    await browser.pause(1000);
+  });
 
   afterEach(async function (browser) {
     await browser.pause(1000); // give some time for events to be sent
